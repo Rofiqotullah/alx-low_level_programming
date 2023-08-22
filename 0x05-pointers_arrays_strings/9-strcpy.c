0df3ac765addb3469877ac4a940bbf6beb02244e
Badaru Rofiqotullah <Rofiqotullah.badaru@gmail.com>
2023-08-22 11:41:02 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * char *_strcpy - a function that copies the string pointer
- * y src
+ * char *_strcpy - a function that copies the string pointed to by src
  * @dest: copy to
  * @src: copy from
  * Return: string
