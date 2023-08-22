4a6fb818e257f9d937830f38e67691c80be98c4d
Badaru Rofiqotullah <Rofiqotullah.badaru@gmail.com>
2023-08-22 11:37:54 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -24,6 +24,7 @@ int _atoi(char *s)
 	{
 		if (s[i] == '-')
 			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -37,7 +38,9 @@ int _atoi(char *s)
 		}
 		i++;
 	}
+
 	if (f == 0)
 		return (0);
+
 	return (n);
 }
